6a070064c2dce6d5de03f3eddd73793cdf78aa3a
naurisievins <nauris.ievins@gmail.com>
2023-01-14 19:00:49 +0200

    1 task left

--- src/scripts.ts
+++ src/scripts.ts
@@ -4,6 +4,7 @@ const btn3 = document.querySelector<HTMLElement>('.btn__3');
 const btn4 = document.querySelector<HTMLElement>('.btn__4');
 const btn5 = document.querySelector<HTMLElement>('.btn__5');
 const btn6 = document.querySelector<HTMLElement>('.btn__6');
+const btn7 = document.querySelector<HTMLElement>('.btn__7');
 
 const box1 = document.querySelector<HTMLElement>('.box__1');
 const box2 = document.querySelector<HTMLElement>('.box__2 .change_text');
@@ -11,6 +12,11 @@ const box3 = document.querySelector<HTMLElement>('.box__3');
 const box4 = document.querySelector<HTMLElement>('.box__4');
 const box5 = document.querySelector<HTMLElement>('.box__5');
 const box6 = document.querySelector<HTMLElement>('.box__6 .timer1');
+const allBoxes = document.querySelectorAll<HTMLElement>('.box');
+
+const output = document.querySelector<HTMLElement>('.output');
+const timer2 = document.querySelector<HTMLElement>('.timer2');
+const inputField = document.querySelector<HTMLInputElement>('.input_field');
 
 const colors: string[] = ['#0d1c03', '#2c401b', '#586f31', '#94a84b', '#e2ea69'];
 
@@ -57,6 +63,16 @@ btn5?.addEventListener('click', () => {
   box5.style.backgroundColor = randomColor(colors);
 });
 
-btn6?.addEventListener('click', () => {
-  startCounter();
+btn6?.addEventListener('click', startCounter);
+
+btn7?.addEventListener('click', () => {
+  allBoxes.forEach(box => {
+    box.style.backgroundColor = '#18D5E1';
+  });
+  document.body.style.backgroundColor = '#000000';
+  output.style.color = 'white';
+});
+
+inputField?.addEventListener('input', () => {
+  output.innerHTML = inputField.value;
 });
